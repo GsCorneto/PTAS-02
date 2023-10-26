b6b4d48c8ff351065bf43377cea66616cb193a34
GsCorneto <131817217+GsCorneto@users.noreply.github.com>
2023-10-26 13:46:49 -0400

    mudei

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.get('/', (req, res) => {
 });
 
 const produtos = [
-  {id: 1, nome: "Gorgonzola", preco: "R$100", descricao: "descricao"},
-  {id: 2, nome: "Provolone", preco: "R$78", descricao: "descricao"},
-  {id: 3, nome: "Gouda", preco: "R$", descricao: "descricao"},
-  {id: 4, nome: "Parmesão", preco: "R$100", descricao: "descricao"},
-  {id: 5, nome: "Coalho", preco: "R$100", descricao: "descricao"},
-  {id: 6, nome: "Muçarela", preco: "R$68", descricao: "descricao"},
-  {id: 7, nome: "Parmesão", preco: "R$50", descricao: "descricao"},
-  {id: 8, nome: "Cheddar", preco: "R$98", descricao: "descricao"},
-  {id: 9, nome: "Lua Cheia", preco: "R$63", descricao: "descricao"},
-  {id: 10, nome: "Brie", preco: "R$54", descricao: "descricao"},
+  {id: 1, nome: "Gorgonzola", preco: "R$100", descricao: "Queijo azul, com sabor mais puxado para o salgado. Ótimo com várias receitas."},
+  {id: 2, nome: "Provolone", preco: "R$78", descricao: "Ótimo como acompanhamento ou porção."},
+  {id: 3, nome: "Gouda", preco: "R$", descricao: "Queijo com sabor mais doce,  ótimo acompanhado de geleia e nozes."},
+  {id: 4, nome: "Parmesão", preco: "R$100", descricao: "Muito utilizado em finalizar macarrão."},
+  {id: 5, nome: "Coalho", preco: "R$100", descricao: "Queijo comumente utilizado em receitas nordestinas."},
+  {id: 6, nome: "Muçarela", preco: "R$68", descricao: "Queijo delicioso para lanches e receitas, extremamente gostoso."},
+  {id: 7, nome: "Roquefort", preco: "R$50", descricao: "Queijo tijolo"},
+  {id: 8, nome: "Cheddar", preco: "R$98", descricao: "Queijo alaranjado com sabor único, muito utilizado em lanches"},
+  {id: 9, nome: "Lua Cheia", preco: "R$63", descricao: "Sabor doce"},
+  {id: 10, nome: "Brie", preco: "R$54", descricao: ""},
 ]
 
 
